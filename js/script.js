f5a4769acbd1584f99bc1fa1f7d1406526303e64
Dragan <milosevdragan1988@gmail.com>
2021-12-29 15:53:31 +0100

    g
    Signed-off-by: Dragan <milosevdragan1988@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,23 +1,2 @@
 // eslint-disable-next-line no-unused-vars
 const projectName = "survey form";
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
